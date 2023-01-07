c2808bcfe8f0abbebfeaf566b797c76e47fd926f
sslthn31 <alhambra.sulthan@gmail.com>
2023-01-07 20:47:49 +0700

    new mongodb configuration

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 4000
-const database = process.env.MONGO_URI || "mongodb://user31:nFOnlCJc58ozLVRE@cluster0-shard-00-00.rujyh.mongodb.net:27017,cluster0-shard-00-01.rujyh.mongodb.net:27017,cluster0-shard-00-02.rujyh.mongodb.net:27017/blogDb?ssl=true&replicaSet=atlas-wm4uqb-shard-0&authSource=admin&retryWrites=true&w=majority"
+const database = process.env.MONGO_URI || "mongodb+srv://user31:xJOOq7wPf55Yrv4A@cluster0.rujyh.mongodb.net/?retryWrites=true&w=majority"
 
 
 const authRoutes = require('./src/routes/auth');
